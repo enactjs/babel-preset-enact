871d01189361106ba036ad85bc911fd2a5dd20ee
taeyoung.hong <taeyoung.hong@lge.com>
2023-04-05 11:04:20 +0900

    remove unnecessary rules in .eslintrc.js and remove unused modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,13 +12,7 @@ module.exports = {
 		commonjs: true,
 		node: true
 	},
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:prettier/recommended',
-		'prettier'
-	],
+	extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
 	parser: '@babel/eslint-parser',
 	parserOptions: {
 		ecmaFeatures: {
@@ -28,13 +22,7 @@ module.exports = {
 		sourceType: 'module',
 		requireConfigFile: false
 	},
-	plugins: ['@babel', 'jsx-a11y', 'react', 'enact', 'import'],
-	settings: {
-		react: {
-			pragma: 'React', // Pragma to use, default to "React"
-			version: 'detect' // React version, default to the latest React stable release
-		}
-	},
+	plugins: ['@babel', 'import'],
 	rules: {
 		'block-scoped-var': 'warn',
 		curly: ['warn', 'multi-line'],
@@ -110,72 +98,6 @@ module.exports = {
 		'use-isnan': 'warn',
 		'wrap-iife': ['error', 'inside'],
 
-		// jsx-a11y plugin https://github.com/evcohen/eslint-plugin-jsx-a11y
-		'jsx-a11y/alt-text': 'warn',
-		'jsx-a11y/anchor-has-content': 'warn',
-		'jsx-a11y/anchor-is-valid': [
-			'warn',
-			{
-				aspects: ['noHref', 'invalidHref']
-			}
-		],
-		'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
-		'jsx-a11y/aria-props': 'warn',
-		'jsx-a11y/aria-proptypes': 'warn',
-		'jsx-a11y/aria-role': ['warn', {ignoreNonDOM: true}],
-		'jsx-a11y/aria-unsupported-elements': 'warn',
-		'jsx-a11y/heading-has-content': 'warn',
-		'jsx-a11y/iframe-has-title': 'warn',
-		'jsx-a11y/img-redundant-alt': 'warn',
-		'jsx-a11y/no-access-key': 'warn',
-		'jsx-a11y/no-distracting-elements': 'warn',
-		'jsx-a11y/no-redundant-roles': 'warn',
-		'jsx-a11y/role-has-required-aria-props': 'warn',
-		'jsx-a11y/role-supports-aria-props': 'warn',
-		'jsx-a11y/scope': 'warn',
-
-		// react plugin https://github.com/yannickcr/eslint-plugin-react
-		'react/display-name': 'off',
-		'react/no-access-state-in-setstate': 'warn',
-		'react/no-children-prop': 'warn',
-		'react/no-danger': 'warn',
-		'react/no-deprecated': 'warn',
-		'react/no-did-mount-set-state': ['warn'],
-		'react/no-did-update-set-state': ['warn'],
-		'react/no-direct-mutation-state': 'warn',
-		'react/no-find-dom-node': 'warn',
-		'react/forbid-foreign-prop-types': 'warn',
-		'react/no-is-mounted': 'warn',
-		'react/no-render-return-value': 'warn',
-		'react/no-string-refs': 'warn',
-		'react/no-this-in-sfc': 'warn',
-		'react/no-unescaped-entities': 'warn',
-		'react/no-unknown-property': 'warn',
-		'react/prefer-es6-class': ['warn', 'always'],
-		'react/prop-types': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'react/require-render-return': 'warn',
-		'react/self-closing-comp': 'warn',
-
-		// react plugin - jsx rules
-		'react/jsx-boolean-value': ['warn', 'never'],
-		'react/jsx-key': 'off',
-		'react/jsx-no-bind': [
-			'warn',
-			{
-				ignoreRefs: true,
-				allowArrowFunctions: false,
-				allowBind: false
-			}
-		],
-		'react/jsx-no-comment-textnodes': 'warn',
-		'react/jsx-no-duplicate-props': 'warn',
-		'react/jsx-no-target-blank': 'warn',
-		'react/jsx-no-undef': 'warn',
-		'react/jsx-pascal-case': 'warn',
-		'react/jsx-uses-react': 'off',
-		'react/jsx-uses-vars': 'warn',
-
 		// babel plugin https://github.com/babel/babel/tree/main/eslint/babel-eslint-plugin
 		'@babel/new-cap': [
 			'error',
@@ -185,9 +107,6 @@ module.exports = {
 			}
 		],
 		'@babel/no-unused-expressions': 'warn',
-
-		// enact plugin https://github.com/enactjs/eslint-plugin-enact/
-		'enact/no-module-exports-import': 'error',
 
 		'import/no-unresolved': ['error', {commonjs: true, caseSensitive: true}],
 		'import/named': 'error',
