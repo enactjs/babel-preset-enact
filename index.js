05a7e74537d72642a7ce65bf456874799d99b47c
alexandru-morariu-lgp <84004128+alexandrumorariu@users.noreply.github.com>
2026-07-02 14:42:47 +0300

    Added babel-plugin-react-compiler

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ module.exports = function (api) {
 			[require('@babel/preset-typescript').default]
 		],
 		plugins: [
+			require('babel-plugin-react-compiler'),
 			// Stage 0
 			// '@babel/plugin-proposal-function-bind',
 
