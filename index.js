9a99d4fd4faa6d0e50d1e68351434d8fb67ce7d5
alexandru-morariu-lgp <84004128+alexandrumorariu@users.noreply.github.com>
2026-07-08 17:15:19 +0300

    Review fix

--- index.js
+++ index.js
@@ -26,14 +26,12 @@ const reactVersion = (() => {
 		return null;
 	}
 })();
-const reactMajor = reactVersion ? parseInt(reactVersion.split('.')[0], 10) : 19;
-
-// Opt-in flag, mirrors the ES5 pattern already used in this file
-const useReactCompiler = process.env.REACT_COMPILER === 'true';
+const reactMajor = parseInt(reactVersion.split('.')[0], 10);
 
 module.exports = function (api) {
 	const env = process.env.BABEL_ENV || process.env.NODE_ENV;
 	const es5Standalone = process.env.ES5 && process.env.ES5 !== 'false';
+	const useReactCompiler = (reactMajor >= 19) && !es5Standalone;
 
 	if (api && api.cache) api.cache(() => env + es5Standalone + useReactCompiler);
 
@@ -79,12 +77,7 @@ module.exports = function (api) {
 		],
 		plugins: [
 			// React Compiler must run first in the plugin pipeline
-			useReactCompiler && [
-				require('babel-plugin-react-compiler'),
-				{
-					...(reactMajor < 19 ? {target: '18'} : {})
-				}
-			],
+			useReactCompiler && require('babel-plugin-react-compiler'),
 
 			// Stage 0
 			// '@babel/plugin-proposal-function-bind',
